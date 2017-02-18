1b350423a946f8c6494819c86f440c3879b376f7
Keisuke Isono <keisuke@reirou.jp>
2017-02-18 22:06:24 +0900

    gofmt and refactot

--- handler.go
+++ handler.go
@@ -1,57 +1,61 @@
 package main
 
 import (
-  "net/http"
-  "io"
-  "io/ioutil"
-  "os"
-  "os/user"
-  "strings"
-  "bytes"
-  "time"
+	"bytes"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/user"
+	"strings"
+	"time"
 
-  "github.com/aws/aws-sdk-go/aws/signer/v4"
-  "github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/signer/v4"
 )
 
 func handler(endpoint string) func(w http.ResponseWriter, r *http.Request) {
-  u, _ := user.Current()
-  creds := credentials.NewSharedCredentials(u.HomeDir + "/.aws/credentials", os.Getenv("AWS_PROFILE"))
-  signer := v4.NewSigner(creds)
-  client := http.DefaultClient
-  return func(w http.ResponseWriter, r *http.Request) {
-    url := r.URL
-    url.Host = endpoint
-    url.Scheme = "https"
-    req, _ := http.NewRequest(r.Method, url.String(), r.Body)
-    req.ContentLength = r.ContentLength;
-    for k, vs := range r.Header {
-      if k == "Connection" {
-        continue;
-      }
-      for _, v := range vs {
-        req.Header.Add(k, strings.Replace(v, "http://localhost:8080", "https://" + endpoint, -1))
-      }
-    }
-    var body io.ReadSeeker
-    if req.Body != nil {
-      buf, _ := ioutil.ReadAll(req.Body)
-      body = bytes.NewReader(buf)
-    }
-    _, err := signer.Sign(req, body, "es", os.Getenv("AWS_REGION"), time.Now())
-    res, err := client.Do(req)
-  	if err != nil {
-      http.Error(w, err.Error(), http.StatusInternalServerError)
-  		return
-  	}
-    b, err := ioutil.ReadAll(res.Body)
-    if err == nil {
-      for k, v := range res.Header {
-        s := strings.Join(v, ", ")
-        w.Header().Set(k, strings.Replace(s, "http://localhost:8080", "https://" + endpoint, -1))
-      }
-      w.WriteHeader(res.StatusCode)
-      w.Write(b)
-    }
-  }
+	u, _ := user.Current()
+	creds := credentials.NewSharedCredentials(u.HomeDir+"/.aws/credentials", os.Getenv("AWS_PROFILE"))
+	signer := v4.NewSigner(creds)
+	client := http.DefaultClient
+	return func(w http.ResponseWriter, r *http.Request) {
+		url := r.URL
+		url.Host = endpoint
+		url.Scheme = "https"
+		req, _ := http.NewRequest(r.Method, url.String(), r.Body)
+		req.ContentLength = r.ContentLength
+		for k, vs := range r.Header {
+			if k == "Connection" {
+				continue
+			}
+			for _, v := range vs {
+				req.Header.Add(k, strings.Replace(v, "http://localhost:8080", "https://"+endpoint, -1))
+			}
+		}
+		var body io.ReadSeeker
+		if req.Body != nil {
+			buf, _ := ioutil.ReadAll(req.Body)
+			body = bytes.NewReader(buf)
+		}
+		region := os.Getenv("AWS_REGION")
+		if region == "" {
+			region = os.Getenv("AWS_DEFAULT_REGION")
+		}
+		_, err := signer.Sign(req, body, "es", region, time.Now())
+		res, err := client.Do(req)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		b, err := ioutil.ReadAll(res.Body)
+		if err == nil {
+			for k, v := range res.Header {
+				s := strings.Join(v, ", ")
+				w.Header().Set(k, strings.Replace(s, "http://localhost:8080", "https://"+endpoint, -1))
+			}
+			w.WriteHeader(res.StatusCode)
+			w.Write(b)
+		}
+	}
 }
--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-  "log"
-  "net/http"
-  "os"
-  "fmt"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 
-  flag "github.com/ogier/pflag"
+	flag "github.com/ogier/pflag"
 
-  "github.com/aws/aws-sdk-go/aws"
-  "github.com/aws/aws-sdk-go/aws/session"
-  "github.com/aws/aws-sdk-go/service/elasticsearchservice"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/elasticsearchservice"
 )
 
 // options
@@ -33,60 +33,64 @@ var profile string
 var help bool
 
 func main() {
-  // defaultRegion := os.Getenv("AWS_REGION")
-  // if defaultRegion == "" {
-  //   defaultRegion = os.Getenv("AWS_DEFAULT_REGION")
-  // }
-  // defaultProfile := os.Getenv("AWS_PROFILE")
+	flag.StringVar(&endpoint, "endpoint", "", "The Amazon Elasticsearch Service Endpoint to use. ex. search-[domain]-xxxxxx.[region].es.amazonaws.com")
+	flag.StringVar(&domain, "domain", "", "The Amazon Elasticsearch Service Domain to retrive endpoint.")
+	flag.StringVar(&listen, "listen", "127.0.0.1:9200", "Listen on host:port. If you want to connect any address. :9200 or 0.0.0.0:9200")
+	flag.StringVar(&profile, "profile", "", "Use a specific profile from your aws credential file.")
+	flag.StringVar(&region, "region", "", "The region to use. Overrides config/env settings.")
+	flag.BoolVar(&help, "help", false, "show this message.")
+	flag.Parse()
 
-  flag.StringVar(&endpoint, "endpoint", "", "The Amazon Elasticsearch Service Endpoint to use. ex. search-[domain]-xxxxxx.[region].es.amazonaws.com")
-  flag.StringVar(&domain,   "domain",   "", "The Amazon Elasticsearch Service Domain to retrive endpoint.")
-  flag.StringVar(&listen,   "listen",   "127.0.0.1:9200", "Listen on host:port. If you want to connect any address. :9200 or 0.0.0.0:9200")
-  flag.StringVar(&profile,  "profile",  "", "Use a specific profile from your aws credential file.")
-  flag.StringVar(&region,   "region",   "", "The region to use. Overrides config/env settings.")
-  flag.BoolVar(&help,       "help",     false, "show this message.")
-  flag.Parse()
+	if help {
+		flag.Usage()
+		os.Exit(0)
+	}
 
-  if help {
-    flag.Usage()
-    os.Exit(0)
-  }
+	if endpoint == "" && domain == "" {
+		fmt.Fprintln(os.Stderr, "Either --endpoint or --domain is required.")
+		flag.Usage()
+		os.Exit(-1)
+	}
 
-  if endpoint == "" && domain == "" {
-    fmt.Fprintln(os.Stderr, "--endpoint or --domain is required.")
-    flag.Usage()
-    os.Exit(-1)
-  }
+	if endpoint != "" && domain != "" {
+		fmt.Fprintln(os.Stderr, "Both --endpoint and --domain is not be enabled at once.")
+		flag.Usage()
+		os.Exit(-1)
+	}
 
-  envProfile := os.Getenv("AWS_PROFILE")
-  if profile == "" && envProfile != "" {
-    profile = os.Getenv("AWS_PROFILE")
-  }
+	if profile != "" {
+		os.Setenv("AWS_PROFILE", profile)
+	}
 
-  if region == "" &&
+	if region != "" {
+		os.Setenv("AWS_REGION", region)
+	}
 
-  if endpoint == "" {
-    sess, err := session.NewSession()
-    if err != nil {
-      log.Println("failed to create session,", err)
-      return
-    }
-    es := elasticsearchservice.New(sess)
-    params := &elasticsearchservice.DescribeElasticsearchDomainInput{
-  		DomainName: aws.String(domain), // Required
-  	}
-  	resp, err := es.DescribeElasticsearchDomain(params)
-  	if err != nil {
-  		log.Println(err.Error())
-  		return
-  	}
-    processing := *resp.DomainStatus.Processing;
-    endpoint = *resp.DomainStatus.Endpoint
-    if processing || endpoint == "" {
-  		log.Println("This domain is being initialized.")
-  		return
-    }
-  }
-  http.HandleFunc("/", handler(endpoint))
-  http.ListenAndServe("localhost:8080", nil)
+	if endpoint == "" {
+		sess, err := session.NewSession()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Failed to create aws session.")
+			os.Exit(-1)
+		}
+		es := elasticsearchservice.New(sess)
+		params := &elasticsearchservice.DescribeElasticsearchDomainInput{
+			DomainName: aws.String(domain),
+		}
+		resp, err := es.DescribeElasticsearchDomain(params)
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
+		processing := *resp.DomainStatus.Processing
+		endpoint = *resp.DomainStatus.Endpoint
+		if processing && endpoint == "" {
+			fmt.Fprintln(os.Stderr, "This domain is being initialized. Try again later.")
+			return
+		}
+		fmt.Fprintln(os.Stdout, "Using endpoint "+endpoint)
+	}
+
+  fmt.Fprintln(os.Stdout, "Listen "+listen)
+	http.HandleFunc("/", handler(endpoint))
+  http.ListenAndServe(listen, nil)
 }
